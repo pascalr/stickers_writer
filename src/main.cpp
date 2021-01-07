e9c3a59008f10cf37a654babc8f5f63a4bb7f04f
Dark Krael <dark.krael@gmail.com>
2021-01-07 09:37:42 -0500

    .

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,3 @@
-/*This source code copyrighted by Lazy Foo' Productions (2004-2020)
-and may not be redistributed without written permission.*/
-
-//Using SDL and standard IO
 #include <SDL.h>
 #include <stdio.h>
 #include <SDL_image.h>
@@ -35,38 +31,6 @@ bool saveScreenshotBMP(std::string filepath) {
   SDL_SaveBMP(sshot, filepath.c_str());
   SDL_FreeSurface(sshot);
   return true;
-//    SDL_Surface* saveSurface = NULL;
-//    SDL_Surface* infoSurface = NULL;
-//    infoSurface = SDL_GetWindowSurface(SDLWindow);
-//    if (infoSurface == NULL) {
-//        std::cerr << "Failed to create info surface from window in saveScreenshotBMP(string), SDL_GetError() - " << SDL_GetError() << "\n";
-//    } else {
-//        unsigned char * pixels = new (std::nothrow) unsigned char[infoSurface->w * infoSurface->h * infoSurface->format->BytesPerPixel];
-//        if (pixels == 0) {
-//            std::cerr << "Unable to allocate memory for screenshot pixel data buffer!\n";
-//            return false;
-//        } else {
-//            if (SDL_RenderReadPixels(SDLRenderer, &infoSurface->clip_rect, infoSurface->format->format, pixels, infoSurface->w * infoSurface->format->BytesPerPixel) != 0) {
-//                std::cerr << "Failed to read pixel data from SDL_Renderer object. SDL_GetError() - " << SDL_GetError() << "\n";
-//                delete[] pixels;
-//                return false;
-//            } else {
-//                saveSurface = SDL_CreateRGBSurfaceFrom(pixels, infoSurface->w, infoSurface->h, infoSurface->format->BitsPerPixel, infoSurface->w * infoSurface->format->BytesPerPixel, infoSurface->format->Rmask, infoSurface->format->Gmask, infoSurface->format->Bmask, infoSurface->format->Amask);
-//                if (saveSurface == NULL) {
-//                    std::cerr << "Couldn't create SDL_Surface from renderer pixel data. SDL_GetError() - " << SDL_GetError() << "\n";
-//                    delete[] pixels;
-//                    return false;
-//                }
-//                SDL_SaveBMP(saveSurface, filepath.c_str());
-//                SDL_FreeSurface(saveSurface);
-//                saveSurface = NULL;
-//            }
-//            delete[] pixels;
-//        }
-//        SDL_FreeSurface(infoSurface);
-//        infoSurface = NULL;
-//    }
-//    return true;
 }
 
 // Source:
